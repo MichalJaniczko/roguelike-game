fe40c8418c5ca6cbdcc692176e67c66db5b33b99
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-05-08 14:03:39 +0200

    Different files, my own branch

---
+++ main.py
@@ -0,0 +1,21 @@
+from get_char import getch
+from movment import move
+from player import insert_player
+from board import *
+from random import randint
+import os
+
+
+def main():
+    x_pos = 5
+    y_pos = 15
+    character = ""
+    while character != "\\":
+        character = getch()
+        os.system("clear")
+        x_pos, y_pos = move(x_pos, y_pos, character, forestmap)
+        board_with_player = insert_player(forestmap, x_pos, y_pos)
+        print_board(board_with_player)
+
+#if __name__ == __main__:
+main()
---
+++ movment.py
@@ -0,0 +1,23 @@
+def move(x_pos, y_pos, character, forestmap):
+    """Function responsible for movement player,
+    get paramet character and when it equal to "wasd" change
+    position by defined value.
+    Function prevent cross boarder of game area.
+    """
+
+
+    forestmap[y_pos][x_pos] = "."
+    if character == "w":
+        if forestmap[y_pos - 1][x_pos] == ".":
+            y_pos -= 1
+    if character == "s":
+        if forestmap[y_pos + 1][x_pos] == ".":
+            y_pos += 1
+    if character == "a":
+        if forestmap[y_pos][x_pos - 1] == ".":
+            x_pos -= 1
+    if character == "d":
+        if forestmap[y_pos][x_pos + 1] == ".":
+            x_pos += 1
+    return x_pos, y_pos
+
